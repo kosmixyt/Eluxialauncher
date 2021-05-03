89f3880c74869e44e5b0beaf7e64655e4feeecf4
kosmixyt <67480533+kosmixyt@users.noreply.github.com>
2021-05-03 20:36:54 +0200

    v0.5
    modification

--- app/assets/js/scripts/uicore.js
+++ app/assets/js/scripts/uicore.js
@@ -49,10 +49,11 @@ if(!isDev){
                 loggerAutoUpdaterSuccess.log('New update available', info.version)
                 
                 if(process.platform === 'darwin'){
-                    info.darwindownload = `https://github.com/dscalzi/HeliosLauncher/releases/download/v${info.version}/helioslauncher-setup-${info.version}.dmg`
+
+                    info.darwindownload = `https://github.com/kosmixyt/Eluxialauncher/releases/download/v${info.version}/helioslauncher-setup-${info.version}.dmg`
                     showUpdateUI(info)
                 }
-                
+
                 populateSettingsUpdateInformation(info)
                 break
             case 'update-downloaded':
